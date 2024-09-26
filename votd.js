a06401ad469ad003c580145470c5c50a6c96203c
RJ <richsouth@protonmail.com>
2024-09-26 15:30:50 +0100

    Initial Commit

---
+++ votd.js
@@ -0,0 +1,29 @@
+const papa = require("papaparse");
+const fs = require("fs");
+const file = fs.createReadStream("almanac-2024.csv");
+
+var today = new Date(Date.now() - 86400000);
+// var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+today = dd + "/" + mm + "/" + yyyy;
+
+papa.parse(file, {
+  complete: function (results) {
+    for (let i = 0; i < results.data.length; i++) {
+      if (results.data[i][1] == today) {
+        console.log(results.data[i][3]);
+      }
+    }
+  },
+});
